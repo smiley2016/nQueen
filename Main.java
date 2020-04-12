6e45600b5a1b52a9fe35d3117e4f87c7acb922c0
smiley2016 <38744116+smiley2016@users.noreply.github.com>
2020-04-12 19:10:10 +0300

    Gyorsabb

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
 
@@ -7,7 +8,7 @@ public class Main {
     private static ArrayList<Integer> rightDiagonal = new ArrayList<>();
     private static ArrayList<Queen> q = new ArrayList<>();
     private static ArrayList<ArrayList<Integer>> q1 = new ArrayList<>();
-    private static int n = 4;
+    private static int n = 100;
 
     static class Queen{
         private int col;
@@ -16,12 +17,6 @@ public class Main {
 
         Queen(int col) {
             this.col = col;
-        }
-
-        public Queen(int col, int left, int right) {
-            this.col = col;
-            this.left = left;
-            this.right = right;
         }
 
         int getCol() {
@@ -49,8 +44,15 @@ public class Main {
         }
     }
 
-
     public static void main(String[] args) {
+        System.out.print("Adja meg a kiralynok szamat: ");
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+
+        for (int i = 0; i<n; ++i){
+            q.add(new Queen(i));
+        }
+
         for(int i = 0; i<n; ++i){
             q1.add(i,new ArrayList<>());
         }
@@ -60,29 +62,41 @@ public class Main {
                 q1.get(i).add(0);
             }
         }
+
         do{
             int nSwaps;
             randomPermutation(n);
             do{
                 nSwaps = 0;
-                for (int i = 0; i<q.size(); ++i){
-                    for (int j = i+1; j<q.size(); ++j){
+                for (int i = 0; i<n; ++i){
+                    for (int j = i+1; j<n; ++j){
                         if(is_attacked(i) || is_attacked(j)){
-                            if(isSwappable(i,j)){
-                                performSwap(i,j);
-                                nSwaps++;
+                            boolean check = worthSwapping(i,j);
+                            if(check){
+                                nSwaps+=1;
                             }
                         }
                     }
                 }
-                if(!checkCollision()){
-                    break;
-                }
-            }while (nSwaps == 0);
+            }while (nSwaps != 0);
         }while (checkCollision());
 
-        System.out.println("Kesz");
+        System.out.println("Done");
 
+        System.out.print("Queens: ");
+
+        for (Queen i : q) {
+            System.out.print(i.getCol()+ " ");
+        }
+        System.out.println();
+
+        System.out.println("Diagonals");
+        print(rightDiagonal);
+        print(leftDiagonal);
+
+        System.out.println("Solved matrix");
+
+        printMatrix();
     }
 
     private static boolean is_attacked(int x) {
@@ -106,23 +120,40 @@ public class Main {
         }
     }
 
-    private static boolean isSwappable(int i, int j){
-        int count = 0;
-        for(int k = 0; k<leftDiagonal.size(); ++k){
-            count += leftDiagonal.get(k);
-            count += rightDiagonal.get(k);
+    private static boolean worthSwapping(int i, int j){
+        int before = 0;
+        int after = 0;
+        for(int k = 0; k<2*n-1; ++k){
+            if(leftDiagonal.get(k) > 1){
+                before+=leftDiagonal.get(k)-1;
+            }
+            if(rightDiagonal.get(k) > 1){
+                before+=rightDiagonal.get(k)-1;
+            }
         }
+
+//        System.out.println("before swap " + before);
         performSwap(i, j);
-        for(int k = 0; k<leftDiagonal.size(); ++k){
-            count -= leftDiagonal.get(k);
-            count -= rightDiagonal.get(k);
+        calculateDiagonals();
+        for(int k = 0; k<2*n-1; ++k){
+            if(leftDiagonal.get(k) > 1){
+                after+=leftDiagonal.get(k)-1;
+            }
+            if(rightDiagonal.get(k) > 1){
+                after+=rightDiagonal.get(k)-1;
+            }
         }
-        if(count > 0){
-            performSwap(j, i);
-            return false;
+        //      System.out.println("after swap " + after);
+//        System.out.println("rightdiagonal:");
+//        print(rightDiagonal);
+//        System.out.println("leftdiagonal:");
+//        print(leftDiagonal);
+        if(before > after){
+            return true;
         }else{
             performSwap(j, i);
-            return true;
+            calculateDiagonals();
+            return false;
         }
     }
 
@@ -132,21 +163,20 @@ public class Main {
         q1.get(col2).set(q.get(col2).getCol(),0);
         q1.get(col2).set(q.get(col1).getCol(),1);
 
-        int temp = q.get(col1).getLeft();
-        int temp2 = q.get(col1).getRight();
+        int tempLeft = q.get(col1).getLeft();
+        int tempRight = q.get(col1).getRight();
         q.get(col1).setLeft(q.get(col2).getLeft());
         q.get(col1).setRight(q.get(col2).getRight());
-        q.get(col2).setLeft(temp);
-        q.get(col2).setRight(temp2);
+        q.get(col2).setLeft(tempLeft);
+        q.get(col2).setRight(tempRight);
 
+        int temp = q.get(col1).getCol();
         q.get(col1).setCol(q.get(col2).getCol());
-        q.get(col2).setCol(q.get(col1).getCol());
-
-        calculateDiagonals();
+        q.get(col2).setCol(temp);
     }
 
     private static boolean checkCollision() {
-        for(int i = 0; i< leftDiagonal.size(); ++i){
+        for(int i = 0; i< 2*n-1; ++i){
             if(leftDiagonal.get(i) > 1  || rightDiagonal.get(i) > 1){
                 return true;
             }
@@ -155,55 +185,32 @@ public class Main {
     }
 
     private static void randomPermutation(int n){
+        for(int i = 0; i<n; ++i){
+            q1.get(i).set(q.get(i).getCol(),0);
+        }
+
+        Collections.shuffle(q);
         leftDiagonal.clear();
         rightDiagonal.clear();
-        q.clear();
-        q1.clear();
-
-        for(int i = 0; i<n; ++i){
-            q1.add(i,new ArrayList<>());
-        }
-
-        for (int i = 0; i<n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                q1.get(i).add(0);
-            }
-        }
-
-        for (int i = 0; i<n; ++i){
-            q.add(new Queen(-1));
-        }
 
         for(int i = 0; i<2*n-1; ++i){
             leftDiagonal.add(0);
             rightDiagonal.add(0);
         }
 
-
         for(int i = 0; i<n; ++i){
-            q.get(i).setCol(i);
-        }
-
-        Collections.shuffle(q);
-
-        for(int i = 0; i<n; ++i){
-            q1.get(i).set(q.get(i).getCol(), 1);
-        }
-
-        for(int i = 0; i<n; ++i){
-            for(int j = 0; j<n; ++j){
-                System.out.print(q1.get(i).get(j)+" ");
-            }
-            System.out.println();
+            q1.get(i).set(q.get(i).getCol(),1);
         }
 
         calculateDiagonals();
-
-        print(rightDiagonal);
-        print(leftDiagonal);
     }
 
     private static void calculateDiagonals(){
+        for (int i =0 ; i < 2*n-1; ++i){
+            leftDiagonal.set(i,0);
+            rightDiagonal.set(i,0);
+        }
+
         for( int k = 0 ; k < n * 2 ; k++ ) {
             for( int j = 0 ; j <= k ; j++ ) {
                 int i = k - j;
